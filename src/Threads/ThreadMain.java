a2e8ed906e9a376ccf9265688554d7e27ba7eac3
Gunathilaka <anjurashmika188@gmail.com>
2024-05-16 16:17:16 +0530

    Combined all the treads as a task calling join method from Java Thread class

--- src/Threads/ThreadMain.java
+++ src/Threads/ThreadMain.java
@@ -1,7 +1,7 @@
 package Threads;
 
 public class ThreadMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         SayHi hi = new SayHi();
         SayHello hello = new SayHello();
@@ -20,5 +20,33 @@ public class ThreadMain {
 
         t1.start();
         t2.start();
+
+        //Create a new thread Using Anonymous Inner class
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Ano Hi");
+                }
+            }
+        });
+        t3.start();
+
+        //Create a thread using Lambda Expression
+        Thread t4 = new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Lambda Hi");
+            }
+        });
+        t4.start();
+
+        /*You have to activated deadlock for threads which are needs to process
+        all the threads via calling join method*/
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
     }
 }
