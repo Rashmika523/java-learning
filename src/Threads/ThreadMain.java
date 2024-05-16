e0eced8145c83ed0cf1a03ada8da85a35dcfde25
Gunathilaka <anjurashmika188@gmail.com>
2024-05-16 16:03:26 +0530

    crated Hi and Hello Objects and Created two thread objects from Java Thread class

--- src/Threads/ThreadMain.java
+++ src/Threads/ThreadMain.java
@@ -9,5 +9,13 @@ public class ThreadMain {
         //Calling start method to run threads. This method came from Java Thread class
         hi.start();
         hello.start();
+
+        //Create objects from Hi and Hello class
+        Hi hi1 = new Hi();
+        Hello hello1 = new Hello();
+
+        //Created two thread object to call start method
+        Thread t1 = new Thread(hi1);
+        Thread t2 = new Thread(hello1);
     }
 }
