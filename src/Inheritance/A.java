2464616510d05e7e9afa5b1149d089f4c6ff91b8
Gunathilaka <anjurashmika188@gmail.com>
2024-05-20 23:07:06 +0530

    create one instance variable and static variable in class A. try this key word in class A Constructor

--- src/Inheritance/A.java
+++ src/Inheritance/A.java
@@ -1,14 +1,19 @@
 package Inheritance;
 
 public class A {
-    int a;
-    A(){
-        System.out.println("A()");
+    int a = 10;
+    static int b = 20;
+
+    A() {
+        //  this(a); // error
+        /*You can't use instance variable with this keyword
+         * because at this time this object is not initialized*/
+        this(b);
+        /*You can use static variables with "this" keyword */
     }
-    A(int i){
+
+    A(int i) {
         System.out.println("A(int i)");
     }
-    A(int i,int y){
-        System.out.println("A(int i,int y");
-    }
+
 }
