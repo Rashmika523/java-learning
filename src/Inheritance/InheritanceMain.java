6386e9870942497bad5826bbcd7b3040c76720a6
Gunathilaka <anjurashmika188@gmail.com>
2024-05-20 22:52:33 +0530

    created anonymous objects from class B related to all constructors

--- src/Inheritance/InheritanceMain.java
+++ src/Inheritance/InheritanceMain.java
@@ -3,7 +3,11 @@ package Inheritance;
 public class InheritanceMain {
     public static void main(String[] args) {
 
-        new D();
+        new B();
+        System.out.println("=============");
+        new B(10);
+        System.out.println("==============");
+        new B(10,20);
 
     }
 }
