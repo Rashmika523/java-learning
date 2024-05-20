debb7a93b47dd36021d9af5f7a77999af5f52d65
Gunathilaka <anjurashmika188@gmail.com>
2024-05-20 22:54:50 +0530

    added supper keywords in all constructors in class B

--- src/Inheritance/B.java
+++ src/Inheritance/B.java
@@ -5,12 +5,15 @@ public class B extends A{
     int b;
 
     B(){
+        super();
         System.out.println("B()");
     }
     B(int i){
+        super(10);
         System.out.println("B(int i)");
     }
     B(int i,int y){
+        super(10,20);
         System.out.println("B(int i,int y");
     }
 
