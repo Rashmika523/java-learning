3f9d025f71c2cc1bdafbb415cd34d97e1cfb509d
Gunathilaka <anjurashmika188@gmail.com>
2024-05-20 22:37:21 +0530

    added static block in base class (Class B)

--- src/Inheritance/B.java
+++ src/Inheritance/B.java
@@ -1,16 +1,15 @@
 package Inheritance;
 
 public class B extends A{
+
     int b;
+
     public void printB(){
         System.out.println("b :"+b);
     }
-    public void callPrint(){
-        printA(); //illegal
-        printB(); //legal
+
+    static {
+        System.out.println("Static Block B");
     }
-    public void printAB(){
-        System.out.println("a :"+a); //legal
-        System.out.println("b :"+b); //legal
-    }
+
 }
