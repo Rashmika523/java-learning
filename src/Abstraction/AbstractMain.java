4f528dc69e65c9960a155799f1ca456114b7dc7c
Gunathilaka <anjurashmika188@gmail.com>
2024-05-27 22:32:27 +0530

    Tried to create an object using Abstract bike class

--- src/Abstraction/AbstractMain.java
+++ src/Abstraction/AbstractMain.java
@@ -3,5 +3,9 @@ package Abstraction;
 public class AbstractMain {
     public static void main(String[] args) {
 
+        // Bike bike = new Bike(); error
+        /*We can't create an object using abstract class. Abstract class also have constructor,
+        but we can't initialize object using that constructor */
+
     }
 }
