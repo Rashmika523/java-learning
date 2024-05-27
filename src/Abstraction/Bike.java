48b4caf406c19bf07c436deed62d344b2c37ba47
Gunathilaka <anjurashmika188@gmail.com>
2024-05-27 22:23:32 +0530

    we can add implemented method in abstract class

--- src/Abstraction/Bike.java
+++ src/Abstraction/Bike.java
@@ -4,4 +4,7 @@ abstract public class Bike {
     abstract public void jump();
     //abstract public static void run();
     /*we can't use abstract key word with static methods*/
+    public void handle(){
+        System.out.println("Handling...!");
+    }
 }
