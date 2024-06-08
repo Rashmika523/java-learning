b846764304fa1db946f6c1b09fc006dff6ebcd8e
Gunathilaka <anjurashmika188@gmail.com>
2024-06-08 12:02:34 +0530

    Print total value of based on the given number by user.

--- src/Recursion/RecursionMain.java
+++ src/Recursion/RecursionMain.java
@@ -2,15 +2,16 @@ package Recursion;
 
 public class RecursionMain {
 
-    public static int sun(int number){
+    public static int sum(int number){
         if(number>0){
-            return number+sun(number-1);
+            return number+sum(number-1);
         }else {
             return 0;
         }
     }
 
     public static void main(String[] args) {
-
+        int total= sum(10);
+        System.out.println("Results is :"+total);
     }
 }
