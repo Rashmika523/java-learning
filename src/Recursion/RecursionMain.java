16799db36386ec3468865cdadaef885691b00880
Gunathilaka <anjurashmika188@gmail.com>
2024-06-08 11:58:36 +0530

    The added sum method calculated the total based on the given number.

--- src/Recursion/RecursionMain.java
+++ src/Recursion/RecursionMain.java
@@ -1,6 +1,15 @@
 package Recursion;
 
 public class RecursionMain {
+
+    public static int sun(int number){
+        if(number>0){
+            return number+sun(number-1);
+        }else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
