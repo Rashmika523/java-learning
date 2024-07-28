5365a917b067ef2879b2a0f2a97a31896a4d556f
rashmika523 <anjurashmika188@gmail.com>
2024-07-28 09:45:57 +0530

    call displaydata via legacyUI

--- src/DesignPatterns/Adapter/Initialize.java
+++ src/DesignPatterns/Adapter/Initialize.java
@@ -3,5 +3,8 @@ package DesignPatterns.Adapter;
 public class Initialize {
     public static void main(String[] args) {
 
+        AppServer legacyUI = new LegacyAppUI();
+        legacyUI.displayData(new XMLData());
+
     }
 }
